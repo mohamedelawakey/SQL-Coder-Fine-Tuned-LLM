3cd532b7bc04d258e426a2d59fce8860b9a9a52b
mohamedelawakey <mohamedelawakey@gmail.com>
2026-09-18 04:19:38 +0300

    move pipeline into versions

--- pipeline/clean.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# drop duplicates
-def clean_data(data):
-    print("Before:", data.shape)
-    data = data.drop_duplicates()
-    print("After:", data.shape)
-
-    return data
